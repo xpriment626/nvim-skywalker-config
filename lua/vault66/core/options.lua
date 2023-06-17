9714f5f23fc1e6ed654cd78e46ba780d0c98f3ef
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-17 23:23:52 +0800

    config plugins, themes, keybindings

---
+++ lua/vault66/core/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.autoindent = true
+opt.expandtab = true
+opt.wrap = false
+opt.ignorecase = true
+opt.smartcase = true
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+opt.backspace = "indent,eol,start"
+opt.clipboard:append("unnamedplus")
+opt.splitright = true
+opt.splitbelow = true
+opt.iskeyword:append("-")
+
---
+++ lua/vault66/plugins/lualine.lua
@@ -0,0 +1,11 @@
+-- import lualine plugin safely
+local status, lualine = pcall(require, "lualine")
+if not status then
+  return
+end
+
+lualine.setup({
+  options = {
+    theme = "kanagawa",
+  },
+})
