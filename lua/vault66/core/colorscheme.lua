c038f280305603423f1aaede7ab3234819d49f90
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-20 14:02:21 +0800

    refactor: colorscheme config

--- lua/vault66/core/colorscheme.lua
+++ lua/vault66/core/colorscheme.lua
@@ -1,1 +1,14 @@
-vim.cmd("colorscheme kanagawa-dragon")
+require('kanagawa').setup({
+    disable_background = true
+})
+
+function ColorMyPencils(color) 
+	color = "kanagawa"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
